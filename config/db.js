46978d8e575812bef0c35d6e9a897c9f01709ee4
Sumith Ekanayake <sumithe@embla.asia>
2020-10-13 10:40:07 +0530

    add comments

--- config/db.js
+++ config/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
+//Connect to the database
 const connectDB = async () => {
     try {
         await mongoose.connect(db, {
